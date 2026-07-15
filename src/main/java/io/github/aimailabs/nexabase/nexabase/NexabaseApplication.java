fa55a0bad6709198f08832005214332c57967bab
Aimony <aimony@qq.com>
2026-07-15 11:40:26 +0800

    feat: init project

---
+++ src/main/java/io/github/aimailabs/nexabase/nexabase/NexabaseApplication.java
@@ -0,0 +1,13 @@
+package io.github.aimailabs.nexabase.nexabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexabaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/aimailabs/nexabase/nexabase/NexabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.aimailabs.nexabase.nexabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
